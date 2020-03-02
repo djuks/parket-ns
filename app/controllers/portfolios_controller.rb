60441fd35500e485f9b9a4654e44a9ad7b04246d
strahinja <djuricastrahinja@gmail.com>
2020-03-02 11:16:45 +0100

    Add active storage config

---
+++ app/controllers/portfolios_controller.rb
@@ -0,0 +1,69 @@
+class PortfoliosController < ApplicationController
+  before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
+
+  # GET /portfolios
+  # GET /portfolios.json
+  def index
+    @portfolios = Portfolio.all
+  end
+
+  # GET /portfolios/1
+  # GET /portfolios/1.json
+  def show
+  end
+
+  # GET /portfolios/new
+  def new
+    @portfolio = Portfolio.new
+  end
+
+  # GET /portfolios/1/edit
+  def edit
+  end
+
+  # POST /portfolios
+  # POST /portfolios.json
+  def create
+    @portfolio = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio.save
+        format.html { redirect_to @portfolio }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  # PATCH/PUT /portfolios/1
+  # PATCH/PUT /portfolios/1.json
+  def update
+    respond_to do |format|
+      if @portfolio.update(portfolio_params)
+        format.html { redirect_to @portfolio }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  # DELETE /portfolios/1
+  # DELETE /portfolios/1.json
+  def destroy
+    @portfolio.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_url }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_portfolio
+      @portfolio = Portfolio.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def portfolio_params
+      params.require(:portfolio).permit(:title, :description)
+    end
+end
